cfc5165b11ce867ad07227660e30d9e6376f002b
WBradbeer <willbradbeer@gmail.com>
2015-04-13 21:28:31 -0400

    PEP8 Style and main function
    Applied PEP8 style guide to functions and variables. Created main
    function so doesn't run on import.

--- retrieve_url.py
+++ retrieve_url.py
@@ -2,26 +2,26 @@ from urllib import request
 import sched, time
 
 ##Downloads a specific URL with filename based on time
-def downloadXML(URL, filename):
-    dl = request.URLopener()
-    dl.retrieve(URL, filename + str(time.time()) + ".xml")
+def download_xml(URL, filename):
+    download = request.URLopener()
+    download.retrieve(URL, filename + str(time.time()) + ".xml")
     print("File downloaded at ")
     print(time.gmtime())
     print(" UTC")
-    
-url = "http://dd.weather.gc.ca/citypage_weather/xml/ON/s0000430_e.xml"
 
-    
-s = sched.scheduler(time.time, time.sleep)
 
 ##Downloads a common URL at specified time intervals
-def scheduledDownload(url, interval, count):
+def scheduled_download(url, interval, count):
+    schedule = sched.scheduler(time.time, time.sleep)
     for j in range(count):
-        s.enter(interval*j,1,downloadXML, (url, 'Data' ))
-    s.run()
-    
-
-scheduledDownload(url, 3600, 24)
+        schedule.enter(interval*j,1,download_xml, (url, 'Data' ))
+    schedule.run()
 
 
+def main():
+    url = "http://dd.weather.gc.ca/citypage_weather/xml/ON/s0000430_e.xml"
+    scheduled_download(url, 3600, 24)
 
+
+if __name__ == '__main__':
+    main()
