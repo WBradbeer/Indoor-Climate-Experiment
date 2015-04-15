ebb9ebcdec13054ed590f7769a93199069f90a60
WBradbeer <willbradbeer@gmail.com>
2015-04-14 20:51:19 -0400

    Style update and main function added
    Followed PEP8 style guide

--- read_serial.py
+++ read_serial.py
@@ -1,46 +1,42 @@
 import serial 
 import time 
-##Header required fir time stamp
-Header = "T"
 
-##Writes current time to Arduino
-def sendTimeStamp(header, time):
-    timeStr = str(t)
-    timeStamp = header + timeStr
-    ser.write(timeStamp.encode())
-    
-## Boolean for whether Arduino is connected
-connected = False
+#Header is required for time stamp to be read by arduino
+header = "T"
 
-##Open Arduino Serial Port
-ser = serial.Serial("COM4", 9600)
-  
+def send_time_stamp(header, time):
+    time_string = str(t)
+    time_stamp = header + time_string
+    ser.write(time_stamp.encode())
 
-##Loop until Arduino is ready
-while not connected:
-    serin = ser.read()
-    connected = True
-##Gives seconds from 1970 to current time UTC 
-t = time.time()
-sendTimeStamp(Header, t)    
-    
-##Open text file
-textFile = open("read.txt", 'w')
-currentTime = str(time.ctime())
-textFile.write("********************")
-textFile.write('Started reading serial at: ')
-textFile.write(currentTime)
-textFile.write("********************")
 
-##read serial to text file
-while 1:
-    if ser.inWaiting():
-        x=ser.read()
-        x= x.decode("utf-8")
-        print(x)
-        textFile.write(x)
-        textFile.flush()
-	
+def main():    
+    connected = False
+    #Open Arduino Serial Port and loop until arduino is ready
+    ser = serial.Serial("COM4", 9600)   
+    while not connected:
+        serin = ser.read()
+        connected = True
 
-textFile.close()
-ser.close()
+    epoch_time = time.time()
+    send_time_stamp(Header, epoch_time)
+
+    text_file = open("read.txt", 'w')
+    current_time = str(time.ctime())
+    text_file.write("********************")
+    text_file.write('Started reading serial at: ')
+    text_file.write(current_time)
+    text_file.write("********************")
+
+    while 1:
+        if ser.inWaiting():
+            x=ser.read()
+            x= x.decode("utf-8")
+            print(x)
+            text_file.write(x)
+            text_file.flush()
+    text_file.close()
+    ser.close()
+
+if __name__ == '__main__':
+    main()
